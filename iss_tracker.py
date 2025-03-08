9506eeb2fde5bcc4a535177e6811f8178c299a81
Ubuntu <ubuntu@coe332-vm-38.js2local>
2025-03-08 18:00:27 +0000

    added redis database connection

--- iss_tracker.py
+++ iss_tracker.py
@@ -9,28 +9,24 @@ import math as m
 import time
 from flask import Flask, request
 import json
+import redis
 
-app = Flask(__name__)
 
+# xml_dict['ndm']['oem']['header']['CREATION_DATE'] # gets the creation date of data
+
+
+
+# Parsing Arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('-l', '--loglevel', type=str, required=False, default='WARNING',
                     help='set log level to DEBUG, INFO, WARNING, ERROR, or CRITICAL')
-args = parser.parse_args()
+args = parser.parse_args() 
 
+# Setting logging format 
 format_str=f'[%(asctime)s {socket.gethostname()}] %(filename)s:%(funcName)s:%(lineno)s - %(levelname)s: %(message)s'
 logging.basicConfig(level=args.loglevel, format=format_str)
 
-try: 
-    response = requests.get(url='https://nasa-public-data.s3.amazonaws.com/iss-coords/current/ISS_OEM/ISS.OEM_J2K_EPH.xml') 
-    if response.status_code!=200: # return status code --> 200 is success 
-        logging.error(f'Status Error: {response.status_code}') 
-        raise Exception(f'Status Error: {response.status_code}') 
-    xml_dict = xmltodict.parse(response.content) # parses data from xml to dictionary
-    logging.debug(f'Data has been successfully written to data as a {type(xml_dict)}\n')
-    data = xml_dict['ndm']['oem']['body']['segment']['data']['stateVector'] 
-    # each index is its own dictionary with EPOCH, X, Y, Z, X_DOT, Y_DOT, Z_DOT --> each one has @units and #text 
-except FileNotFoundError: 
-    logging.error(f'The specified key does not exist\n') 
+app = Flask(__name__) 
 
 @app.route('/epochs', methods=['GET'])
 def get_epochs(): # gets data from url 
@@ -125,34 +121,6 @@ def now_speed():
     now_dict["units"] = "km/s"
     return [now_dict]
 
-def range_of_data(list_of_dicts: List[dict], keys: List[str]):
-    """
-    This function gets a list of dictionaries with metadata segment. 
-    It prints a statement about the range of data using timestamps 
-    from the first and last epochs.
-
-    Args: 
-        list_of_dicts (list): A list of dictionaries, each dictionary 
-                              should have the specified keys. 
-        keys (list): A list of strings representing the keys for the start 
-                     and end epoch. (i.e. ['START_TIME','STOP_TIME']) 
-
-    Returns:
-        NONE
-    """  
-
-    try: 
-        if len(list_of_dicts)==0:
-            logging.error('Given list was empty')
-            raise ValueError
-        print(f'The provided data has a start time of: {list_of_dicts[keys[0]]},') 
-        print(f'and an end time of: {list_of_dicts[keys[1]]}\n')
-    except KeyError as e: 
-        logging.error(f'Dictionary key not found: {e}\n') 
-    except IndexError as e:
-        logging.error(f'Index out of range: {e}\n')
-
-
 def now_epoch(list_of_dicts: List[dict],epoch_key: str) -> List[dict]: 
     """
     This function gets a list of dictionaries with a time stamp, state vectors
@@ -190,79 +158,45 @@ def now_epoch(list_of_dicts: List[dict],epoch_key: str) -> List[dict]:
     except IndexError as e:
         logging.error(f'Index out of range: {e}\n')
 
+def fetch_latest_iss_data():
+    """Fetches the latest ISS ephemeris data and updates only if new data is available."""
 
-def speed_calc(list_of_dicts: List[dict], keys_for_dict: List[str], 
-               keys_for_data: List[str]) -> dict: 
-    """
-    This function gets a list of dictionaries with velocity components in 
-    Cartesian coordinates. It calculates the average of the speed over the 
-    whole data set, as well as the instantaneous speed closest to "now". 
-
-    Args: 
-        list_of_dicts (list): A list of dictionaries, each dictionary 
-                              should have the specified keys. 
-        keys_for_dict (list): A list of strings for the keys that 
-                                  pertain to velocities in the x, y, z 
-                                  direction and epoch. (i.e.['X_DOT', 
-                                  'Y_DOT', 'Z_DOT', 'EPOCH']) 
-        keys_for_data (list): A list of strings for the keys petaining 
-                              to the units and value of velocity. 
-                              (i.e. ['#units','#text'])
-
-    Returns:
-        dict: Returns the average of speeds and the now speed in a 
-                      dictionary. (i.e. {"avg_speed": "value1", 
-                      "now_speed": "value2"})
-    """  
-    try:
-        if not list_of_dicts: logging.warning(print('Given list was empty'))
-        # initializing values    
-        now_time = time.mktime(time.gmtime())
-        diff = m.inf # initialized as infinity
-        now_speed = None
-        list_of_speeds = []
-
-        for item in list_of_dicts:
-            # Extracting values
-            x_dot = float(item[keys_for_dict[0]][keys_for_data[1]])
-            y_dot = float(item[keys_for_dict[1]][keys_for_data[1]])
-            z_dot = float(item[keys_for_dict[2]][keys_for_data[1]])
-            # Calculating values
-            speed = m.sqrt(m.pow(x_dot,2)+m.pow(y_dot,2)+m.pow(z_dot,2))
-            logging.debug(f'Speed calculated successfully\n')
-            list_of_speeds.append(speed) # saving speeds to list
-            # Calculating time
-            # used ChatGPT to fix 000Z to %fZ in striptime func
-            t = time.mktime(time.strptime(item[keys_for_dict[3]], 
-                                  '%Y-%jT%H:%M:%S.%fZ')) 
-            if abs(now_time-t)<diff: 
-                now_speed = speed
-                diff = abs(now_time-t)
-        avg = sum(list_of_speeds)/len(list_of_dicts)
-
-        return {"avg_speed": avg, "now_speed": now_speed}
-    except KeyError as e: 
-        logging.error(f'Dictionary key not found: {e}\n') 
-    except IndexError as e:
-        logging.error(f'Index out of range: {e}\n')
-
+def get_iss_data():
+    # Getting data from NASA's Website using the requests library 
+    try: 
+        response = requests.get(url='https://nasa-public-data.s3.amazonaws.com/iss-coords/current/ISS_OEM/ISS.OEM_J2K_EPH.xml') 
+        response_head = requests.head(url='https://nasa-public-data.s3.amazonaws.com/iss-coords/current/ISS_OEM/ISS.OEM_J2K_EPH.xml')
+        if response.status_code!=200: # return status code --> 200 is success 
+            logging.error(f'Status Error: {response.status_code}') 
+            raise Exception(f'Status Error: {response.status_code}') 
+        xml_dict = xmltodict.parse(response.content) # parses data from xml to dictionary
+        logging.debug(f'Data has been successfully written to data as a {type(xml_dict)}\n')
+        data = xml_dict['ndm']['oem']['body']['segment']['data']['stateVector'] 
+        # each index is its own dictionary with EPOCH, X, Y, Z, X_DOT, Y_DOT, Z_DOT --> each one has @units and #text 
+        return response_head.headers['Last-Modified'], data # --> returns as tuple 
+    except FileNotFoundError: 
+        logging.error(f'The specified key does not exist\n') 
 
 def main():
-    # Prints statement about range of data using timestamps from first to last epochs
-    range_of_data(xml_dict['ndm']['oem']['body']['segment']['metadata'],['START_TIME','STOP_TIME'])
 
-    # Prints full epoch closest to now
-    closest = now_epoch(data,'EPOCH')
-    print(f'Full epoch closest to now is:')
-    print(f'{closest}\n')
+    # if redis database empty then request data and write it to database
+    rd=redis.Redis(host='127.0.0.1', port=6379, db=0)
+    # check number of keys if empty it will return 0
+    response_head = requests.head(url='https://nasa-public-data.s3.amazonaws.com/iss-coords/current/ISS_OEM/ISS.OEM_J2K_EPH.xml')
+    header_time = response_head.headers['Last-Modified']
+    # if header time is not the same then request new data
+    if len(rd.keys())==0 or rd.get('Last-Modified') is not header_time: # order matters since an empty list cannot have a Last-modified time
+        data = get_iss_data() 
+        # write data to database inside if statement
+        rd.set('Last-Modified',data[0]) # sets the last-modified value for reference
+        # for loop to write each EPOCH to database for easier lookup         
+        for item in data[1]: 
+            rd.set(str(item['EPOCH']),json.dumps(item)) 
+    # once the container is running, if it keeps running then check header every 24 hours
+    # this will ensure you have most up to date data 
 
-    # calculates and prints average speed and instantaneous speed closest to now
-    speed = speed_calc(data,['X_DOT','Y_DOT','Z_DOT','EPOCH'],['#units','#text'])
-    units = data[0]['X_DOT']['@units']
-    print(f'Average speed and instantaenous speed closest to now are:')
-    print(f'{speed} {units}\n')
-    
+
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
     main()
+    app.run(debug=True, host='0.0.0.0')
