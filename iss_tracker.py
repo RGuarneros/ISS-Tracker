70d9a5516f3f39d690904fc95e8f929387282c26
Ubuntu <ubuntu@coe332-vm-38.js2local>
2025-03-08 21:42:13 +0000

    finished updating old routes

--- iss_tracker.py
+++ iss_tracker.py
@@ -10,11 +10,10 @@ import time
 from flask import Flask, request
 import json
 import redis
-
-
-# xml_dict['ndm']['oem']['header']['CREATION_DATE'] # gets the creation date of data
-
-
+import threading
+from astropy import coordinates
+from astropy import units
+from astropy.time import Time
 
 # Parsing Arguments
 parser = argparse.ArgumentParser()
@@ -26,7 +25,58 @@ args = parser.parse_args()
 format_str=f'[%(asctime)s {socket.gethostname()}] %(filename)s:%(funcName)s:%(lineno)s - %(levelname)s: %(message)s'
 logging.basicConfig(level=args.loglevel, format=format_str)
 
+rd=redis.Redis(host='127.0.0.1', port=6379, db=0)
+
 app = Flask(__name__) 
+
+def fetch_latest_iss_data():
+    """Fetches the latest ISS ephemeris data and updates only if new data is available."""
+    # if redis database empty then request data and write it to database
+    # check number of keys if empty it will return 0 
+    response_head = requests.head(url='https://nasa-public-data.s3.amazonaws.com/iss-coords/current/ISS_OEM/ISS.OEM_J2K_EPH.xml')
+    header_time = response_head.headers['Last-Modified']
+    # if header time is not the same then request new data
+    if len(rd.keys())==0 or rd.get('Last-Modified').decode('utf-8') != header_time: # order matters since an empty list cannot have a Last-modified time
+        logging.debug('Data was not the same, initializing update.')
+        data = get_iss_data() 
+        # write data to database inside if statement
+        rd.set('Last-Modified',data[0]) # sets the last-modified value for reference
+        # for loop to write each EPOCH to database for easier lookup         
+        for item in data[1]: 
+            rd.set(str(item['EPOCH']),json.dumps(item)) # redis saves it in random order 
+        logging.info('Data has been updated.')
+    else:
+        logging.debug('Data was the same.')
+    # once the container is running, if it keeps running then check header every 24 hours
+    # this will ensure you have most up to date data 
+
+def get_iss_data():
+    # Getting data from NASA's Website using the requests library 
+    try: 
+        response = requests.get(url='https://nasa-public-data.s3.amazonaws.com/iss-coords/current/ISS_OEM/ISS.OEM_J2K_EPH.xml') 
+        response_head = requests.head(url='https://nasa-public-data.s3.amazonaws.com/iss-coords/current/ISS_OEM/ISS.OEM_J2K_EPH.xml')
+        if response.status_code!=200: # return status code --> 200 is success 
+            logging.error(f'Status Error: {response.status_code}') 
+            raise Exception(f'Status Error: {response.status_code}') 
+        xml_dict = xmltodict.parse(response.content) # parses data from xml to dictionary
+        logging.debug(f'Data has been successfully written to data as a {type(xml_dict)}\n')
+        data = xml_dict['ndm']['oem']['body']['segment']['data']['stateVector'] 
+        # each index is its own dictionary with EPOCH, X, Y, Z, X_DOT, Y_DOT, Z_DOT --> each one has @units and #text 
+        return response_head.headers['Last-Modified'], data # --> returns as tuple 
+    except FileNotFoundError: 
+        logging.error(f'The specified key does not exist\n') 
+        raise Exception(f'The specified key does not exist\n')
+
+# AI used to create background updater 
+def background_updater():
+    while True: # infinite loop to keep updating 
+        fetch_latest_iss_data()
+        logging.info('Sleeping for 24 hours before next check...')
+        time.sleep(86400) 
+
+# Start background updater in a separate thread --> AI used 
+threading.Thread(target=background_updater, daemon=True).start()
+
 
 @app.route('/epochs', methods=['GET'])
 def get_epochs(): # gets data from url 
@@ -36,21 +86,26 @@ def get_epochs(): # gets data from url
     according to the limit and offset values given. 
 
     Args: 
-        NONE
+        NONE 
 
     Returns: 
         result (list): Returns the list of dictionaries according to 
                        some limit and offset values.
     """ 
-    limit = request.args.get('limit',len(data)) # default is all dictionary 
+    limit = request.args.get('limit',len(rd.keys())-1) # default is whole database without header
     offset = request.args.get('offset',0) # default is no offset 
     try: 
         limit = int(limit) 
         offset = int(offset) 
     except ValueError: 
         return "Invalid limit/offset parameter; limit/offset must be integers \n" 
-    result = data[offset:offset+limit] # AI used to slice data safely without for loop, its like MATLAB syntax 
-    return result
+    keys = [key.decode('utf-8') for key in rd.keys() if key.decode('utf-8') != "Last-Modified"] # AI used to check for last modified entry
+    keys.sort() # AI mentioned sort function
+    keys_to_get = keys[offset:offset+limit]
+    data = [] 
+    for epoch in keys_to_get:
+        data.append(json.loads(rd.get(epoch).decode('utf-8')))
+    return data
 
 @app.route('/epochs/<epoch>', methods=['GET']) # make a case where epoch is nonexistent
 def get_epoch(epoch):
@@ -65,14 +120,11 @@ def get_epoch(epoch):
     Returns: 
         result (dict): Returns the dictionary with the epoch value you specified. 
     """
-    specific_epoch = None
-    for item in data:
-        if item["EPOCH"] == epoch: 
-            specific_epoch = item 
-            break 
-    if specific_epoch==None:
-        return {"error": f"Epoch '{epoch}' not found"}, 404 # AI used to return 404 error
-    return specific_epoch 
+    try: 
+        specific_epoch = json.loads(rd.get(epoch).decode('utf-8'))
+        return specific_epoch
+    except AttributeError as e:
+        return {"error": f"Epoch '{epoch}' not found"}, 404 # AI used to return 404 error 
 
 @app.route('/epochs/<epoch>/speed', methods=['GET']) 
 def get_epoch_speed(epoch):
@@ -97,7 +149,28 @@ def get_epoch_speed(epoch):
     z_dot = float(specific_epoch["Z_DOT"]["#text"]) 
     # Calculating speed 
     speed = m.sqrt(m.pow(x_dot,2)+m.pow(y_dot,2)+m.pow(z_dot,2)) 
-    return {'value':str(speed), 'units':' km/s'} 
+    return {'speed':str(speed), 'units':' km/s'} 
+
+# @app.route('/epochs/<epoch>/location', methods=['GET']) 
+# def get_epoch_location(epoch):
+#     specific_epoch = json.loads(rd.get(epoch).decode('utf-8'))
+
+#     return {'latitude':lat, 'longitude':lon, 'altitude':alt, 'geoposition':geo_pos}
+
+# def compute_location(epoch):
+#     x = float(epoch['X']['#text'])
+#     y = float(epoch['Y']['#text'])
+#     z = float(epoch['Z']['#text'])
+
+#     # assumes epoch is in format '2024-067T08:28:00.000Z'
+#     this_epoch=time.strftime('%Y-%m-%d %H:%m:%S', time.strptime(sv['EPOCH'][:-5], '%Y-%jT%H:%M:%S'))
+
+#     cartrep = coordinates.CartesianRepresentation([x, y, z], unit=units.km)
+#     gcrs = coordinates.GCRS(cartrep, obstime=this_epoch)
+#     itrs = gcrs.transform_to(coordinates.ITRS(obstime=this_epoch))
+#     loc = coordinates.EarthLocation(*itrs.cartesian.xyz)
+
+#     return loc.lat.value, loc.lon.value, loc.height.value
 
 @app.route('/now', methods=['GET'])
 def now_speed(): 
@@ -114,14 +187,16 @@ def now_speed():
                        speed. 
     """
 
-    now_dict = now_epoch(data,"EPOCH")
-    speed = get_epoch_speed(now_dict["EPOCH"])
-    speed_val = speed["value"]
-    now_dict["instant speed"] = speed_val 
+    keys = [key.decode('utf-8') for key in rd.keys() if key.decode('utf-8') != "Last-Modified"]
+    now_epoch_label = now_epoch(keys)
+    speed = get_epoch_speed(now_epoch_label)
+    speed_val = speed["speed"]
+    now_dict = json.loads(rd.get(now_epoch_label).decode('utf-8'))
+    now_dict["speed"] = speed_val 
     now_dict["units"] = "km/s"
-    return [now_dict]
+    return now_dict 
 
-def now_epoch(list_of_dicts: List[dict],epoch_key: str) -> List[dict]: 
+def now_epoch(list_of_keys: List[str]) -> str: 
     """
     This function gets a list of dictionaries with a time stamp, state vectors
     and velocities. It identifies the closest epoch to "now".
@@ -137,66 +212,24 @@ def now_epoch(list_of_dicts: List[dict],epoch_key: str) -> List[dict]:
     """  
 
     try: 
-        if not list_of_dicts: logging.warning('Given list was empty')
+        if not list_of_keys: logging.warning('Given list was empty')
         # current time
         now_time = time.mktime(time.gmtime())
         diff = m.inf
-        closest_dict = {} 
 
-        for item in list_of_dicts:
+        for epoch in list_of_keys:
             # Calculating time
             # used ChatGPT to fix 000Z to %fZ in striptime func
-            t = time.mktime(time.strptime(item[epoch_key], 
-                                      '%Y-%jT%H:%M:%S.%fZ')) 
+            t = time.mktime(time.strptime(epoch, '%Y-%jT%H:%M:%S.%fZ')) 
             if abs(now_time-t)<diff: 
-                closest_dict = item
+                closest_epoch = epoch
                 diff = abs(now_time-t)
 
-        return closest_dict
+        return closest_epoch
     except KeyError as e: 
         logging.error(f'Dictionary key not found: {e}\n') 
     except IndexError as e:
         logging.error(f'Index out of range: {e}\n')
 
-def fetch_latest_iss_data():
-    """Fetches the latest ISS ephemeris data and updates only if new data is available."""
-
-def get_iss_data():
-    # Getting data from NASA's Website using the requests library 
-    try: 
-        response = requests.get(url='https://nasa-public-data.s3.amazonaws.com/iss-coords/current/ISS_OEM/ISS.OEM_J2K_EPH.xml') 
-        response_head = requests.head(url='https://nasa-public-data.s3.amazonaws.com/iss-coords/current/ISS_OEM/ISS.OEM_J2K_EPH.xml')
-        if response.status_code!=200: # return status code --> 200 is success 
-            logging.error(f'Status Error: {response.status_code}') 
-            raise Exception(f'Status Error: {response.status_code}') 
-        xml_dict = xmltodict.parse(response.content) # parses data from xml to dictionary
-        logging.debug(f'Data has been successfully written to data as a {type(xml_dict)}\n')
-        data = xml_dict['ndm']['oem']['body']['segment']['data']['stateVector'] 
-        # each index is its own dictionary with EPOCH, X, Y, Z, X_DOT, Y_DOT, Z_DOT --> each one has @units and #text 
-        return response_head.headers['Last-Modified'], data # --> returns as tuple 
-    except FileNotFoundError: 
-        logging.error(f'The specified key does not exist\n') 
-
-def main():
-
-    # if redis database empty then request data and write it to database
-    rd=redis.Redis(host='127.0.0.1', port=6379, db=0)
-    # check number of keys if empty it will return 0
-    response_head = requests.head(url='https://nasa-public-data.s3.amazonaws.com/iss-coords/current/ISS_OEM/ISS.OEM_J2K_EPH.xml')
-    header_time = response_head.headers['Last-Modified']
-    # if header time is not the same then request new data
-    if len(rd.keys())==0 or rd.get('Last-Modified') is not header_time: # order matters since an empty list cannot have a Last-modified time
-        data = get_iss_data() 
-        # write data to database inside if statement
-        rd.set('Last-Modified',data[0]) # sets the last-modified value for reference
-        # for loop to write each EPOCH to database for easier lookup         
-        for item in data[1]: 
-            rd.set(str(item['EPOCH']),json.dumps(item)) 
-    # once the container is running, if it keeps running then check header every 24 hours
-    # this will ensure you have most up to date data 
-
-
-
 if __name__ == '__main__':
-    main()
     app.run(debug=True, host='0.0.0.0')
