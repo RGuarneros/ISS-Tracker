535a06bd22c25185d2606bc87c66fc1b582f9b9e
Ubuntu <ubuntu@coe332-vm-38.js2local>
2025-03-13 05:28:55 +0000

    finished tests for each route and function in iss_tracker script

--- test_iss_tracker.py
+++ test_iss_tracker.py
@@ -1,120 +1,121 @@
 #!/usr/bin/env python3
-from iss_tracker import now_epoch, speed_calc, range_of_data
+from iss_tracker import get_iss_data, compute_location, now_epoch
 import pytest 
-import math
-from typing import List
 import requests
 
-response1 = requests.get('http://localhost:5000/epochs')
-epoch = response1.json()[30]['EPOCH']
-response2 = requests.get('http://localhost:5000/epochs/'+epoch)
-response3 = requests.get('http://localhost:5000/epochs/'+epoch+'/speed')
-response4 = requests.get('http://localhost:5000/now')
-response5 = requests.get('http://localhost:5000/epochs/a')
-response6 = requests.get('http://localhost:5000/epochs/a/speed')
+def test_get_iss_data():
+    data = get_iss_data()
+    assert isinstance(data,tuple) == True 
+    assert isinstance(data[0],str) == True 
+    assert isinstance(data[1],list) == True 
 
-def test_epochs_route():
-    assert response1.status_code == 200
-    assert isinstance(response1.json(),list) == True
-
-def test_specific_epoch_route(): 
-    assert response2.status_code == 200
-    assert isinstance(response2.json(),dict) == True
-    assert response5.status_code == 404
-    assert response2.json()["EPOCH"]==epoch
-
-def test_specific_epoch_speed_route():
-    assert response3.status_code == 200
-    assert isinstance(response3.json(),dict) == True
-    assert response6.status_code == 404
-    assert len(response3.json()) == 2
-
-def test_now_route():
-    assert response4.status_code == 200
-    assert isinstance(response4.json(),list) == True
-    assert len(response4.json())==1
-    assert len(response4.json()[0])==9 # 2 added entries for speed
-
-# AI helped create these tests 
-def test_now_epoch():
-    data = [{'EPOCH': '2025-050T23:48:00.000Z', 'X': {'@units': 'km', '#text': '5870.8349256289102'}, 
+def test_compute_location():
+    data = {'EPOCH': '2025-020T00:00:00.000Z', 'X': {'@units': 'km', '#text': '5870.8349256289102'}, 
              'Y': {'@units': 'km', '#text': '-2867.4525176072102'}, 'Z': {'@units': 'km', '#text':
              '1868.30956895554'}, 'X_DOT': {'@units': 'km/s', '#text': '3.62111898852819'}, 'Y_DOT':
              {'@units': 'km/s', '#text': '3.7289968226834902'}, 'Z_DOT': {'@units': 'km/s', '#text':
-             '-5.6274091912002797'}}]
-    assert type(now_epoch(data, 'EPOCH')) == dict # returns a dictionary
-    assert now_epoch(data,'EPOCH') == data[0] # returns only item in data
-    assert now_epoch(data,'EPOC') is None # EPOCH does not exist
+             '-5.6274091912002797'}}
+    result = compute_location(data) 
+    assert isinstance(result,list) == True
+    assert isinstance(result[0],float) == True
+    assert isinstance(result[1],float) == True
+    assert isinstance(result[2],float) == True
+    assert isinstance(result[3],str) == True
+    assert result[3] == "2025-01-20 00:00:00"
 
-def test_now_epoch_exceptions():
-    data = [{'EPOCH': '2025-050T23:48:00.000Z', 'X': {'@units': 'km', '#text': '5870.8349256289102'}, 
-             'Y': {'@units': 'km', '#text': '-2867.4525176072102'}, 'Z': {'@units': 'km', '#text':
-             '1868.30956895554'}, 'X_DOT': {'@units': 'km/s', '#text': '3.62111898852819'}, 'Y_DOT':
-             {'@units': 'km/s', '#text': '3.7289968226834902'}}]
+def test_now_epoch():
+    data = ['2024-020T00:00:00.000Z', '2024-060T00:00:00.000Z', '2025-050T00:00:00.000Z'] 
+    assert now_epoch('2025-020T00:00:00.000Z') == '2025-020T00:00:00.000Z' 
+    assert now_epoch(['2025-020T00:00:00.000Z']) == '2025-020T00:00:00.000Z' 
+    assert now_epoch(data) == '2025-050T00:00:00.000Z' 
+    assert now_epoch([]) == []
     with pytest.raises(TypeError):
-        now_epoch(data,[])
-    with pytest.raises(ValueError):
-        now_epoch([{'EPOCH': '2025-050T23:48:00.000'}], 'EPOCH')
-    with pytest.raises(ValueError):
-        now_epoch([{'EPOCH': '202-050T23:48:00.000'}], 'EPOCH')
+        now_epoch() 
+    assert isinstance(now_epoch(data),str) == True 
+    assert now_epoch(['a','vb']) == None 
 
+def test_get_epochs_route(): # test for query parameters being infinity or something wrong 
+    response = requests.get('http://localhost:5000/epochs') 
+    assert response.status_code==200
+    assert isinstance(response.json(),list) == True 
+    assert isinstance(response.json()[34],dict) == True
+    assert isinstance(response.json()[34]['EPOCH'],str) == True 
+    response = requests.get('http://localhost:5000/epoch')
+    assert response.status_code!=200
+    entries_wanted = 2
+    response = requests.get(f'http://localhost:5000/epochs?limit={entries_wanted}&offset=3') 
+    assert response.status_code==200
+    assert len(response.json())==2 # you only get the number of entries specified
+    response = requests.get(f'http://localhost:5000/epochs?limit={entries_wanted}') 
+    assert response.status_code==200
+    response = requests.get('http://localhost:5000/epochs?limit=999999') 
+    assert len(response.json())<999999
 
-def test_speed_calc():
-    data = [{'EPOCH': '2025-050T23:48:00.000Z', 'X': {'@units': 'km', '#text': '5870.8349256289102'}, 
-             'Y': {'@units': 'km', '#text': '-2867.4525176072102'}, 'Z': {'@units': 'km', '#text':
-             '1868.30956895554'}, 'X_DOT': {'@units': 'km/s', '#text': '3.62111898852819'}, 'Y_DOT':
-             {'@units': 'km/s', '#text': '3.7289968226834902'},'Z_DOT':{'@units': 'km/s', '#text': 
-             '4.7289968226834902'}}]
-    speed = math.sqrt(pow(3.62111898852819,2)+pow(3.7289968226834902,2)+pow(4.7289968226834902,2))
-    assert speed_calc(data,['X_DOT','Y_DOT', 'Z_DOT', 'EPOCH'], ['@units', '#text']) == {"avg_speed": speed, "now_speed": speed} 
-    data = [{'EPOCH': '2025-050T23:48:00.000Z', 'X': {'@units': 'km', '#text': '5870.8349256289102'}, 
-             'Y': {'@units': 'km', '#text': '-2867.4525176072102'}, 'Z': {'@units': 'km', '#text':
-             '1868.30956895554'}, 'X_DOT': {'@units': 'km/s', '#text': '3.62111898852819'}, 'Y_DOT':
-             {'@units': 'km/s', '#text': '3.7289968226834902'},'Z_DOT':{'@units': 'km/s', '#text': 
-             't'}}]
-    assert speed_calc(data,['X_DO','Y_DOT', 'Z_DOT', 'EPOCH'],['@units', '#text']) == None
-    assert speed_calc(data,['X_DOT','Y_DOT', 'Z_DOT', 'EPOCH'],[]) == None 
-        
-def test_speed_calc_exceptions():
-    data = [{'EPOCH': '2025-050T23:48:00.000Z', 'X': {'@units': 'km', '#text': '5870.8349256289102'}, 
-             'Y': {'@units': 'km', '#text': '-2867.4525176072102'}, 'Z': {'@units': 'km', '#text':
-             '1868.30956895554'}, 'X_DOT': {'@units': 'km/s', '#text': 'r'}, 'Y_DOT':
-             {'@units': 'km/s', '#text': '3.7289968226834902'},'Z_DOT':{'@units': 'km/s', '#text': 
-             '4.7289968226834902'}}]
-    with pytest.raises((ZeroDivisionError,ValueError)): 
-        speed_calc(data,['X_DOT','Y_DOT', 'Z_DOT', 'EPOCH'],['@units', '#text'])
-    with pytest.raises(ZeroDivisionError):
-        speed_calc([],['X_DOT','Y_DOT', 'Z_DOT', 'EPOCH'], [])
+def test_get_epochs_route_exceptions():
+    response = requests.get('http://localhost:5000/epochs?limit=9.4') 
+    error_message = 'Invalid limit/offset parameter; limit/offset must be integers \n' 
+    assert response.content.decode('utf-8')==error_message
+    response = requests.get('http://localhost:5000/epochs?limit=9&offset=1.3') 
+    assert response.content.decode('utf-8')==error_message
+    response = requests.get('http://localhost:5000/epochs?limit=9.1&offset=1')
+    assert response.content.decode('utf-8')==error_message
+    response = requests.get('http://localhost:5000/epochs?limit=9.4&offset=1.1')
+    assert response.content.decode('utf-8')==error_message
 
+def test_specific_epoch_route(): 
+    response = requests.get('http://localhost:5000/epochs') 
+    epoch = response.json()[34]['EPOCH'] 
+    response = requests.get(f'http://localhost:5000/epochs/{epoch}') 
+    assert response.status_code==200 
+    assert isinstance(response.json(),dict) == True 
+    assert response.json()['EPOCH'] == epoch 
+    assert isinstance(response.json()['X_DOT'],dict) == True 
+    assert isinstance(response.json()['X_DOT']['#text'],str) == True 
+    # Wrong epoch 
+    response = requests.get(f'http://localhost:5000/epochs/a') 
+    assert isinstance(response.json(),dict)==True
+    assert response.status_code==404 
 
-def test_range_of_data(capfd):
-    data = {'OBJECT_NAME': 'ISS', 'OBJECT_ID': '1998-067-A', 'CENTER_NAME': 'EARTH', 
-             'REF_FRAME': 'EME2000', 'TIME_SYSTEM': 'UTC', 'START_TIME': '2025-048T12:00:00.000Z', 
-             'STOP_TIME': '2025-063T12:00:00.000Z'}
-    assert range_of_data(data,['START_TIME','STOP_TIME']) == None # doesnt return anything
-    # AI used to capture terminal output
-    range_of_data(data,['START_TIME','STOP_TIME'])
-    captured = capfd.readouterr()
-    assert "The provided data has a start time of: 2025-048T12:00:00.000Z," in captured.out 
+def test_specific_epoch_speed_route(): 
+    response = requests.get('http://localhost:5000/epochs') 
+    epoch = response.json()[34]['EPOCH'] 
+    response = requests.get(f'http://localhost:5000/epochs/{epoch}/speed') 
+    assert response.status_code==200 
+    assert isinstance(response.json(),dict) == True 
+    assert isinstance(response.json()['speed'],str) == True 
+    assert isinstance(response.json()['units'],str) == True 
+    # Wrong Epoch 
+    response = requests.get(f'http://localhost:5000/epochs/a/speed') 
+    assert isinstance(response.json(),dict)==True # get the same output as getting epoch error
+    assert response.status_code==404 
 
-def test_range_of_data_exceptions():
-    data = [{'START_TIME': '2024-050T12:00:00.000Z', 'STOP_TIME': '2024-051T12:00:00.000Z'}]
-    with pytest.raises(TypeError):
-        range_of_data(data,['START_TIME','STOP_TIME'])
-    with pytest.raises(TypeError):
-        range_of_data(data, ['START_TIME'])  # Only one key provided
-    with pytest.raises(TypeError):
-        range_of_data(data, ['INVALID_START', 'INVALID_STOP'])
-    data = None
-    with pytest.raises(TypeError):
-        range_of_data(data, ['START_TIME', 'STOP_TIME'])
-    data = ['Not a dictionary']  # Invalid data format
-    with pytest.raises(TypeError):
-        range_of_data(data, ['START_TIME', 'STOP_TIME'])
-    data = [{'START_TIME': '2024-050T12:00:00.000Z'}]  # Missing 'STOP_TIME'
-    with pytest.raises(TypeError):
-        range_of_data(data, ['START_TIME', 'STOP_TIME'])
-    data = []
-    with pytest.raises(ValueError):
-        range_of_data(data, ['START_TIME', 'STOP_TIME'])
+def test_specific_epoch_location_route(): 
+    response = requests.get('http://localhost:5000/epochs') 
+    epoch = response.json()[34]['EPOCH'] 
+    response = requests.get(f'http://localhost:5000/epochs/{epoch}/location') 
+    assert response.status_code==200 
+    assert isinstance(response.json(),dict) == True 
+    assert len(response.json()) == 5 
+    assert type(response.json()['latitude'])==float
+    assert type(response.json()['longitude'])==float 
+    assert type(response.json()['epoch_timestamp'])==str 
+    assert type(response.json()['altitude'])==float 
+    assert type(response.json()['geoposition'])==str 
+    # Wrong epoch
+    response = requests.get(f'http://localhost:5000/epochs/a/location') 
+    assert isinstance(response.json(),dict)==True # get the same output as getting epoch error
+    assert response.status_code==404 
 
+def test_now_route(): 
+    response = requests.get('http://localhost:5000/now') 
+    assert response.status_code==200 
+    assert isinstance(response.json(),dict) == True 
+    assert len(response.json()) == 7 
+    assert type(response.json()['latitude'])==float
+    assert type(response.json()['longitude'])==float 
+    assert type(response.json()['epoch_timestamp'])==str 
+    assert type(response.json()['altitude'])==float 
+    assert type(response.json()['geoposition'])==str 
+    assert type(response.json()['speed'])==str 
+    assert type(response.json()['now_timestamp'])==str 
+    
